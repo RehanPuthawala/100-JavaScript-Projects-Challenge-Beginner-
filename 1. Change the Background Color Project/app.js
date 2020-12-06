38aa0d59354d4c4f010dfc0987d5e6739af512d7
Mo.Rehan <morehanp@gmail.com>
2020-12-06 21:57:26 +0600

    1. Change the Background Color Project Completed

---
+++ 1. Change the Background Color Project/app.js
@@ -0,0 +1,27 @@
+// * DOM Element Selection * //
+
+const changeColorButton = document.querySelector(".change-color");
+const root = document.documentElement;
+
+const primaryColorsArr = ["red", "green", "blue", "Brown", "Olive", "Orange"];
+const secondaryColorsArr = [
+  "cyan",
+  "magneta",
+  "yellow",
+  "Beige",
+  "Orange",
+  "black",
+];
+
+// * Event Handlers * //
+
+changeColorButton.addEventListener("click", (e) => {
+  let randomIdx = Math.trunc(Math.random() * primaryColorsArr.length) + 1;
+
+  root.style.setProperty("--primary-color", `${primaryColorsArr[randomIdx]}`);
+  root.style.setProperty(
+    "--secondary-color",
+    `${secondaryColorsArr[randomIdx]}`
+  );
+
+  console.log();
